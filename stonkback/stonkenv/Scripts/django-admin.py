d3ce4f6327d2753acbe2c25e435039a5a0c5a81e
jls47 <lukeschaefer@gmail.com>
2021-02-19 13:44:16 -0800

    vs is weird

--- stonkback/stonkenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\lukes\Documents\Programming\UWB\502\stonks\stonkback\stonkback\stonkenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
