690918869cd480068ef2a6bd4c4a988361aeea12
jls47 <lukeschaefer@gmail.com>
2021-02-19 13:34:35 -0800

    django init

---
+++ stonkback/stonkenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\lukes\Documents\Programming\UWB\502\stonks\stonkback\stonkback\stonkenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
